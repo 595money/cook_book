7c5b6cb19e5060080eff034710c73910a9cf8202
595money <595money@gmail.com>
2020-03-28 21:01:55 +0800

    Initial commit

---
+++ example-notebook.md
@@ -0,0 +1,25 @@
+---
+jupyter:
+  jupytext:
+    formats: ipynb,md,py:percent
+    text_representation:
+      extension: .md
+      format_name: markdown
+      format_version: '1.2'
+      jupytext_version: 1.4.1
+  kernelspec:
+    display_name: Python 3
+    language: python
+    name: python3
+---
+
+```python
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn 
+```
+
+## Heart dissease project
+---
+This project is about claissifying whether or not a patient has heart disease.
---
+++ example-notebook.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,md,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn 
+
+# %% [markdown]
+# ## Heart dissease project
+# ---
+# This project is about claissifying whether or not a patient has heart disease.
