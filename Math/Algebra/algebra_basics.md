a35fb60d9887aa166ababd77f239539b60d6c41e
595money_pc <595money@gmail.com>
2020-04-08 21:22:34 +0800

    Resolve conflict

--- Math/Algebra/algebra_basics.md
+++ Math/Algebra/algebra_basics.md
@@ -1,7 +1,7 @@
 ---
 jupyter:
   jupytext:
-    formats: ipynb,md,py:percent
+    formats: ipynb,py:percent,md
     text_representation:
       extension: .md
       format_name: markdown
@@ -14,5 +14,25 @@ jupyter:
 ---
 
 ```python
+import pandas as pd
+import numpy as np
+from jupyterthemes import jtplot
+from IPython.core.display import display, HTML
 
+display(HTML("<style>.container { width:80% !important; }</style>"))
+jtplot.style()
 ```
+
+## 代數基礎
+
+
+* inequalities (不等式)
+
+-2x > 20
+x < 20/ -2  
+箭頭方向會改變
+若${\displaystyle c}$c為 正數 且 {\displaystyle a>b}a > b；則 {\displaystyle ac>bc}ac > bc。
+若{\displaystyle c}c為 正數 且 {\displaystyle a<b}a < b；則 {\displaystyle ac<bc}ac < bc 。
+若{\displaystyle c}c為 負數 且 {\displaystyle a>b}a > b；則 {\displaystyle ac<bc}ac < bc 。
+若{\displaystyle c}c為 負數 且 {\displaystyle a<b}a < b；則 {\displaystyle ac>bc}ac > bc。
+
--- Math/Algebra/algebra_basics.py
+++ Math/Algebra/algebra_basics.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,md,py:percent
+#     formats: ipynb,py:percent,md
 #     text_representation:
 #       extension: .py
 #       format_name: percent
@@ -14,3 +15,25 @@
 # ---
 
 # %%
+import pandas as pd
+import numpy as np
+from jupyterthemes import jtplot
+from IPython.core.display import display, HTML
+
+display(HTML("<style>.container { width:80% !important; }</style>"))
+jtplot.style()
+
+# %% [markdown]
+# ## 代數基礎
+
+# %% [markdown]
+# * inequalities (不等式)
+#
+# -2x > 20
+# x < 20/ -2  
+# 箭頭方向會改變
+# 若${\displaystyle c}$c為 正數 且 {\displaystyle a>b}a > b；則 {\displaystyle ac>bc}ac > bc。
+# 若{\displaystyle c}c為 正數 且 {\displaystyle a<b}a < b；則 {\displaystyle ac<bc}ac < bc 。
+# 若{\displaystyle c}c為 負數 且 {\displaystyle a>b}a > b；則 {\displaystyle ac<bc}ac < bc 。
+# 若{\displaystyle c}c為 負數 且 {\displaystyle a<b}a < b；則 {\displaystyle ac>bc}ac > bc。
+#
