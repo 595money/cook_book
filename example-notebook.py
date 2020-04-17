2609b4d84d19a20b149170bcde126d7acb82f756
595money <595money@gmail.com>
2020-04-18 07:07:45 +0800

    First commit
    use ubuntu

--- example-notebook.py
+++ example-notebook.py
@@ -1,12 +1,12 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,md,py:percent
+#     formats: ipynb,py:percent
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.4.1
+#       jupytext_version: 1.4.2
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
